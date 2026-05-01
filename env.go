1f8bab468576b34e8a88b6d1159d41d2f7541d20
Aleksander Kmiecik <32b3@proton.me>
2026-05-01 12:21:52 +0200

    refactor: improve port configuration handling

--- env.go
+++ env.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"html/template"
 	"log"
+	"net"
 	"os"
 	"time"
 )
@@ -11,18 +12,19 @@ import (
 var tpl = template.Must(template.ParseFiles("index.html"))
 
 const (
-	readR   = 5 * time.Second   // max time to read request from the client
-	writeR  = 10 * time.Second  // max time to write response to the client
-	keepA   = 120 * time.Second // max time for connections using TCP Keep-Alive
-	timeout = 10 * time.Second  // max time to complete tasks before shutdown
-	port    = ":3000"
+	readR       = 5 * time.Second   // max time to read request from the client
+	writeR      = 10 * time.Second  // max time to write response to the client
+	keepA       = 120 * time.Second // max time for connections using TCP Keep-Alive
+	timeout     = 10 * time.Second  // max time to complete tasks before shutdown
+	defaultPort = "3000"
 )
 
 func getPort() string {
-	if p := os.Getenv("PORT"); p != "" {
-		return ":" + p
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
 	}
-	return port
+	return net.JoinHostPort("", port)
 }
 
 func parseAPIKey() string {
